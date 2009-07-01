bbf664986cb8e0b6f894e2b119a26cbed72cdc1c
Matthew Todd <matthew.todd@gmail.com>
2009-07-01 12:08:59 +0300

    Require ssl and basic auth.

--- config/rack.rb
+++ config/rack.rb
@@ -1,2 +1,23 @@
+class RequireSSL
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    if env['rack.url_scheme'] == 'https'
+      @app.call(env)
+    else
+      [ 400, {}, 'SSL is required.' ]
+    end
+  end
+end
+
+unless Merb.environment == 'development'
+  use RequireSSL
+  use Rack::Auth::Basic do |api_key, _|
+    api_key == ENV['API_KEY']
+  end
+end
+
 use Merb::Rack::Static, ChefServerSlice.dir_for(:public)
 run Merb::Rack::Application.new
