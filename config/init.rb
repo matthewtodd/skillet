1057c552ac26652b1a6a0517addb86dd438930c9
Matthew Todd <matthew.todd@gmail.com>
2009-07-28 17:15:44 +0300

    Move cookbooks to the same subdir, start role for hcts.

--- config/init.rb
+++ config/init.rb
@@ -14,9 +14,10 @@ end
 
 Merb::BootLoader.before_app_loads do
   Chef::Config.instance_eval do
-    cookbook_path         [Merb.root_path('site-cookbooks'), Merb.root_path('cookbooks')]
+    cookbook_path         [Merb.root_path('cookbooks', 'site'), Merb.root_path('cookbooks', 'skillet'), Merb.root_path('cookbooks', 'opscode')]
     openid_cstore_couchdb true
     openid_store_couchdb  true
+    role_path             Merb.root_path('roles')
     search_index_path     Merb.root_path('tmp', 'search_index')
   end
 
