0553ccd261a1cb9d6154bf4d349b6837345d2edb
Matthew Todd <matthew.todd@gmail.com>
2009-07-29 12:46:26 +0300

    Capture HTTP_HOST for use in chef::client configuration.

--- config/rack.rb
+++ config/rack.rb
@@ -1,35 +1,29 @@
-class RequireSSL
-  def initialize(app)
-    @app = app
-  end
-
+class RequireSSL < Struct.new(:app)
   def call(env)
-    ssl?(env) ? @app.call(env) : bad_request
-  end
-
-  private
-
-  def ssl?(env)
-    env['HTTPS'] == 'on' || env['HTTP_X_FORWARDED_PROTO'] == 'https'
-  end
-
-  def bad_request
-    [ 400, {}, 'SSL is required.' ]
+    if env['HTTPS'] == 'on' || env['HTTP_X_FORWARDED_PROTO'] == 'https'
+      app.call(env)
+    else
+      # I'd rather not redirect; we'll be receiving non-GET requests as well.
+      [ 400, {}, 'SSL is required.' ]
+    end
   end
 end
 
-class RequireBasicAuthentication < Rack::Auth::Basic
-  def realm
-    'The Skillet'
+# Useful for our overridden version of the chef::client config file.
+class CaptureHTTPHost < Struct.new(:app)
+  def call(env)
+    ENV['HTTP_HOST'] ||= env['HTTP_HOST']
+    app.call(env)
   end
 end
 
 unless Merb.environment == 'development'
   use RequireSSL
-  use RequireBasicAuthentication do |access_token, _|
+  use Rack::Auth::Basic do |access_token, _|
     access_token == ENV['APP_ACCESS_TOKEN']
   end
 end
 
+use CaptureHTTPHost
 use Merb::Rack::Static, ChefServerSlice.dir_for(:public)
 run Merb::Rack::Application.new
